d4a0098a104e2b43564289ea215f77aab4777367
Kizito Matemera <kmaterms@gmail.com>
2025-02-25 14:15:39 +0200

    Update 01. Plan caching and reuse.sql
    I added a new comment

--- ch1/01. Plan caching and reuse.sql
+++ ch1/01. Plan caching and reuse.sql
@@ -26,7 +26,7 @@ SELECT LastName, FirstName, MiddleName
 FROM Person.Person
 WHERE PersonType = N'EM' AND BusinessEntityID IN (5, 7, 13, 17, 19);
 GO
-
+--I have read all of this and know it
 -- * The sp_executesql procedure
 EXECUTE sp_executesql @stmt = N'SELECT LastName, 
 FirstName, MiddleName 
